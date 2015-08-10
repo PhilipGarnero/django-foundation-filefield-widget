98f5450f43673de408e0cfdfd3b374e62ef64040
PhilipGarnero <philip.garnero@gmail.com>
2015-08-10 23:47:48 +0200

    added imagefield

--- README.rst
+++ README.rst
@@ -26,11 +26,14 @@ Use the widget in your forms :
 .. code-block:: python
 
     from django import forms
-    from foundation_filefield_widget.widgets import FoundationFileInput
+    from foundation_filefield_widget.widgets import FoundationFileInput, FoundationImageInput
 
     class MyForm(forms.Form):
         my_file = forms.FileField(widget=FoundationFileInput)
+        my_image = forms.FileField(widget=FoundationImageInput)
 
+
+Don't forget to include the media part of your form in your templates.
 
 If you want to easily define forms you should look into `crispy-forms <http://django-crispy-forms.readthedocs.org>`_
 And if you want to define forms styled with Foundation you should also check `crispy-forms-foundation <http://crispy-forms-foundation.readthedocs.org>`_
--- foundation_filefield_widget/widgets.py
+++ foundation_filefield_widget/widgets.py
@@ -3,6 +3,7 @@
 from django import forms
 from django.template.loader import render_to_string
 from django.utils.safestring import mark_safe
+from django.conf import settings
 
 
 class FoundationFileInput(forms.ClearableFileInput):
@@ -22,10 +23,41 @@ class FoundationFileInput(forms.ClearableFileInput):
         id = self.build_attrs(attrs, type=self.input_type, name=name)['id']
         attrs.update({"onchange": "$(this).prev().val(this.value.replace('C:\\\\fakepath\\\\', ''));"
                                   "$(this).prev()[0].onclick = null;".format(id),
-                      "class": attrs.get("class", "") + " compact"})
+                      "class": attrs.get("class", "") + " compact hidden"})
 
         return mark_safe(render_to_string(self.template, {
             'input': super(FoundationFileInput, self).render(name, value, attrs),
             'id': id,
             'value': value
+        }))
+
+
+class FoundationImageInput(forms.ClearableFileInput):
+
+    class Media:
+        css = {'all': ('foundation_filefield_widget/foundation-imagefield.css',)}
+
+    template_with_clear = ''  # We don't need this
+    template_with_initial = '%(input)s'
+
+    def __init__(self, attrs=None, template='foundation_filefield_widget/foundation_imagefield_widget.html'):
+        super(FoundationImageInput, self).__init__(attrs=attrs)
+        self.template = template
+
+    def render(self, name, value, attrs=None):
+        attrs = attrs or {}
+        id = self.build_attrs(attrs, type=self.input_type, name=name)['id']
+        attrs.update({"onchange": "if (this.files && this.files[0]) {{"
+                                  "var that = this;"
+                                  "var reader = new FileReader();"
+                                  "reader.onload = function(e) {{$(that).prev(\"img\").attr('src', e.target.result);}};"
+                                  "reader.readAsDataURL(this.files[0]);"
+                                  "}}".format(id),
+                      "class": attrs.get("class", "") + " compact hidden"})
+
+        return mark_safe(render_to_string(self.template, {
+            'input': super(FoundationImageInput, self).render(name, value, attrs),
+            'id': id,
+            'value': value,
+            'STATIC_URL': settings.STATIC_URL
         }))
